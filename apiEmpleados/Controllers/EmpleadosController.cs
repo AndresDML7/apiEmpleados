b9017a74fcc692ffd5ccf9e061426096f60de879
AndresDML7 <andru2015.7@gmail.com>
2021-09-23 23:37:36 -0500

    Add project files.

---
+++ apiEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,116 @@
+﻿using apiEmpleados.Context;
+using apiEmpleados.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiEmpleados.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmpleadosController : Controller
+    {
+        private readonly AppDbContext context;
+
+        public EmpleadosController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<EmpleadosController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.EMPLEADOS.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET api/<EmpleadosController>/5
+        [HttpGet("{id}", Name = "GetEmpleado")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var empleado = context.EMPLEADOS.FirstOrDefault(e => e.id == id);
+                return Ok(empleado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST api/<EmpleadosController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Empleados_Bd empleado)
+        {
+            try
+            {
+                context.EMPLEADOS.Add(empleado);
+                context.SaveChanges();
+                return CreatedAtRoute("GetEmpleado", new { id = empleado.id }, empleado);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<EmpleadosController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Empleados_Bd empleado)
+        {
+            try
+            {
+                if(empleado.id == id)
+                {
+                    context.Entry(empleado).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetEmpleado", new { id = empleado.id }, empleado);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<EmpleadosController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var empleado = context.EMPLEADOS.FirstOrDefault(e => e.id == id);
+                if(empleado != null)
+                {
+                    context.EMPLEADOS.Remove(empleado);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ apiEmpleados/Models/Empleados_Bd.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiEmpleados.Models
+{
+    public class Empleados_Bd
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int cedula { get; set; }
+
+        public string nombres { get; set; }
+
+        public string apellidos { get; set; }
+
+        public int telefono { get; set; }
+
+        public int celular { get; set; }
+
+        public string email { get; set; }
+
+        public string direccion { get; set; }
+    }
+}
